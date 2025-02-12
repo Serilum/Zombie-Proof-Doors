745131b531705f472cf94e0233166fddd126abc8
Rick South <rick@natamus.com>
2025-02-12 23:05:59 +0100

    (v3.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/zombieproofdoors/ModForge.java
+++ Forge/src/main/java/com/natamus/zombieproofdoors/ModForge.java
@@ -28,7 +28,7 @@ public class ModForge {
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-   		MinecraftForge.EVENT_BUS.register(new ForgeZombieJoinEvent());
+   		MinecraftForge.EVENT_BUS.register(ForgeZombieJoinEvent.class);
 	}
 
 	private static void setGlobalConstants() {

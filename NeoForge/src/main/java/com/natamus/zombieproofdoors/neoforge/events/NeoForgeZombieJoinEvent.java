7db2400b74cbc280cc6e53774e77a979db87442f
Rick South <rick@natamus.com>
2025-10-02 12:16:58 +0200

    Zombie Proof Doors source code for 1.21.9.

---
+++ NeoForge/src/main/java/com/natamus/zombieproofdoors/neoforge/events/NeoForgeZombieJoinEvent.java
@@ -0,0 +1,13 @@
+package com.natamus.zombieproofdoors.neoforge.events;
+
+import com.natamus.zombieproofdoors.events.ZombieJoinEvent;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+public class NeoForgeZombieJoinEvent {
+	@SubscribeEvent
+	public static void onEntityJoin(EntityJoinLevelEvent e) {
+		ZombieJoinEvent.onEntityJoin(e.getLevel(), e.getEntity());
+	}
+}
